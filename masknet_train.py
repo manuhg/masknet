02ec46e74ce4e761cb5a59f3c16f32139a4ecaaf
stas <sheffmail@mail.ru>
2018-02-02 14:05:30 +0300

    batching and weights save

--- masknet_train.py
+++ masknet_train.py
@@ -8,6 +8,7 @@ from keras.layers import TimeDistributed
 from keras.layers import Input
 from keras.layers import LSTM
 from keras.models import Model
+from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam, SGD, RMSprop
 from pycocotools.coco import COCO
 from random import shuffle
@@ -24,25 +25,6 @@ my_num_rois = 32
 my_inp_size = 19
 
 class RoiPoolingConv(Layer):
-    '''ROI pooling layer for 2D inputs.
-    See Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition,
-    K. He, X. Zhang, S. Ren, J. Sun
-    # Arguments
-        pool_size: int
-            Size of pooling region to use. pool_size = 7 will result in a 7x7 region.
-        num_rois: number of regions of interest to be used
-    # Input shape
-        list of two 4D tensors [X_img,X_roi] with shape:
-        X_img:
-        `(1, channels, rows, cols)` if dim_ordering='th'
-        or 4D tensor with shape:
-        `(1, rows, cols, channels)` if dim_ordering='tf'.
-        X_roi:
-        `(1,num_rois,4)` list of rois, with ordering (x,y,w,h)
-    # Output shape
-        3D tensor with shape:
-        `(1, num_rois, channels, pool_size, pool_size)`
-    '''
     def __init__(self, pool_size, num_rois, **kwargs):
 
         self.dim_ordering = K.image_dim_ordering()
@@ -65,30 +47,18 @@ class RoiPoolingConv(Layer):
         img = x[0]
         rois = x[1]
 
-        outputs = []
+        rois_flattened = K.reshape(rois, (-1, 4))
 
-        for roi_idx in range(self.num_rois):
+        shape = tf.shape(rois_flattened)
+        box_indices = tf.range(0, shape[0]) // self.num_rois
 
-            x = rois[0, roi_idx, 0]
-            y = rois[0, roi_idx, 1]
-            w = rois[0, roi_idx, 2]
-            h = rois[0, roi_idx, 3]
+        res = tf.image.crop_and_resize(
+            img, rois_flattened, box_indices, (self.pool_size, self.pool_size),
+            method="bilinear")
 
-            x = K.cast(x, 'int32')
-            y = K.cast(y, 'int32')
-            w = K.cast(w, 'int32')
-            h = K.cast(h, 'int32')
+        res = K.reshape(res, (-1, self.num_rois, self.pool_size, self.pool_size, self.nb_channels))
 
-            rs = tf.image.resize_images(img[:, y:y+h, x:x+w, :], (self.pool_size, self.pool_size))
-            outputs.append(rs)
-
-
-        final_output = K.concatenate(outputs, axis=0)
-
-        final_output = K.reshape(final_output, (1, self.num_rois, self.pool_size, self.pool_size, self.nb_channels))
-        final_output = K.permute_dimensions(final_output, (0, 1, 2, 3, 4))
-
-        return final_output
+        return res
 
 def roi_pool_cpu(frame, bbox, pool_size):
     frame_h, frame_w = frame.shape[:2]
@@ -164,27 +134,17 @@ def process_coco(coco, img_path):
                 if (np.count_nonzero(msk) == 0):
                     continue
 
-                x1 = int(bbox[0] * my_inp_size)
-                y1 = int(bbox[1] * my_inp_size)
-                w1 = int(bbox[2] * my_inp_size)
-                h1 = int(bbox[3] * my_inp_size)
-
-                if (w1 <= 0):
-                    w1 = 1
-                if (h1 <= 0):
-                    h1 = 1
-
                 assert(len(rois) < my_num_rois)
 
-                x1 = np.float32(x1)
-                y1 = np.float32(y1)
-                w1 = np.float32(w1)
-                h1 = np.float32(h1)
+                x1 = np.float32(bbox[0])
+                y1 = np.float32(bbox[1])
+                w1 = np.float32(bbox[2])
+                h1 = np.float32(bbox[3])
 
-                rois.append([x1, y1, w1, h1])
+                rois.append([y1, x1, y1 + h1, x1 + w1])
                 msks.append(msk.astype('float32'))
         for _ in range(my_num_rois - len(rois)):
-            rois.append([np.float32(0.0), np.float32(0.0), np.float32(1.0), np.float32(1.0)])
+            rois.append([np.float32(0.0), np.float32(0.0), np.float32(0.0), np.float32(0.0)])
             msks.append(fake_msk)
         msks = np.array(msks)
         msks = msks[..., np.newaxis]
@@ -226,8 +186,8 @@ def my_loss(y_true, y_pred):
     y_pred = tf.gather(y_pred, ix)
 
     loss = K.binary_crossentropy(target=y_true, output=y_pred)
-    #loss = K.mean(loss)
-    #loss = K.reshape(loss, [1, 1])
+    loss = K.mean(loss)
+    loss = K.reshape(loss, [1, 1])
     return loss
 
 if __name__ == "__main__":
@@ -251,14 +211,14 @@ if __name__ == "__main__":
     train_imgs = process_coco(train_coco, bdir + "/images/train2014")
     val_imgs = process_coco(val_coco, bdir + "/images/val2014")
 
-    batch_size = 1
+    batch_size = 64
 
     train_data = fit_generator(train_coco, train_imgs, batch_size)
 
-    #next(train_data)
-    #aaaaa;
-
     validation_data = fit_generator(val_coco, val_imgs[:100], batch_size)
+
+    callbacks = []
+    callbacks.append(ModelCheckpoint(filepath="weights.hdf5", monitor='val_loss', save_best_only=True))
 
     model.fit_generator(train_data,
         steps_per_epoch=len(train_imgs) / batch_size,
@@ -266,4 +226,7 @@ if __name__ == "__main__":
         epochs=2,
         validation_data=validation_data,
         use_multiprocessing=False,
-        verbose=1)
+        verbose=1,
+        callbacks=callbacks)
+
+    print("Done!")
